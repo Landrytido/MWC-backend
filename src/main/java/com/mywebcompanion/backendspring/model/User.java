258113276434a9d0868449081838edb90e4da471
Landrytido <atla2804@student.eafc-uccle.be>
2025-06-05 10:05:11 +0200

    refactor: Supprimer les annotations LazyToOne pour simplifier le modèle User

--- src/main/java/com/mywebcompanion/backendspring/model/User.java
+++ src/main/java/com/mywebcompanion/backendspring/model/User.java
@@ -7,8 +7,6 @@ import lombok.ToString;
 
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.LazyToOne;
-import org.hibernate.annotations.LazyToOneOption;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
@@ -40,73 +38,61 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<Note> notes = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<SavedLink> links = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<Notebook> notebooks = new ArrayList<>();
 
     // ✅ BlocNote avec LAZY + NO_PROXY
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     @JsonIgnore
     private BlocNote blocNote;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<Comment> comments = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<Task> tasks = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<DailyPlan> dailyPlans = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<DailyTask> dailyTasks = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<DailyTaskHistory> dailyTaskHistory = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<NoteTask> noteTasks = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<LinkGroup> linkGroups = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JsonIgnore
     @BatchSize(size = 10)
-    @LazyToOne(LazyToOneOption.NO_PROXY)
     private List<File> files = new ArrayList<>();
 
     @CreationTimestamp
